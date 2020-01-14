a1384fb10f75f99519255bc1ef63a224a1a3aeb9
Lavstor <45934398+Lavstor@users.noreply.github.com>
2020-01-14 12:05:02 +0300

    Test 001

---
+++ app/src/main/java/ru/myshkin/nikita/android/minesweeper/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.myshkin.nikita.android.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
